59e7ca7ea702818e9ba05facd79758dc72060d45
dimpal0510 <dimpalk.88@gmail.com>
2025-06-03 14:52:57 +0530

    Create web scraping.py

---
+++ we Scraping.py
@@ -0,0 +1,11 @@
+# Create the file (using a text editor or touch command)
+touch new_file.txt
+
+# Stage the file
+git add new_file.txt
+
+# Commit the changes
+git commit -m "Add new file new_file.txt"
+
+# Push the changes to the remote repository
+git push origin main
